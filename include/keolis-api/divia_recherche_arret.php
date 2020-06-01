37923c842fe1e09771acf78b7d5d55636fc8ad5f
qowax <nathan.haudot@outlook.fr>
2020-06-01 17:44:39 +0200

    WIP Form post arrêt
    - La recherche d'arrêt retourne la référence d'arrêt pur l'API, manque la page horaires.php pour mettre les données en forme.

--- include/keolis-api/divia_recherche_arret.php
+++ include/keolis-api/divia_recherche_arret.php
@@ -4,6 +4,21 @@ if (isset($_POST['type-submit'])) {
     //Récupération des infos de la Form
     $arret = $_POST['arret'];
     $direction = $_POST['direction'];
-    echo 'Arrêt: '.$arret.'<br>Direction: '.$direction;
+    $ligne = substr($direction, 0,2);
+    $direction = substr($direction, 5);
+
+    require '../php/dbh.php';
+
+    $stmt = $connection->prepare('SELECT `arret_ref` FROM `cot_divia_arrets` WHERE `arret`= ? AND `direction`= ? AND `ligne_nom`= ?');
+            $stmt->bind_param("sss", $arret, $direction, $ligne);
+            $stmt->execute();
+    
+    $result = $stmt->get_result();
+    
+    $row = $result->fetch_array(MYSQLI_NUM);
+
+    $arret_ref = $row[0];
+
+    echo $arret_ref;
 }
 ?>
