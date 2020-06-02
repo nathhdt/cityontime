3563a7f57d8a1c849600d7f44b9cb477deb247e1
qowax <nathan.haudot@outlook.fr>
2020-06-02 02:03:40 +0200

    Corrections graphiques

--- horaires.php
+++ horaires.php
@@ -1,4 +1,4 @@
-//<?php echo $_GET['ref']; ?>
+<!-- <?php echo $_GET['ref']; ?> -->
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -24,7 +24,7 @@
       <div class="row">
         <div class="container mb-5">
           <div class="section-title col-lg-8 col-md-10 ml-auto mr-auto">
-            <h3 class="section-title text-center my-5" style="color:#fafafa"><?php echo $arret; ?>L3 - Dallas CRI > Epirey Cap Nord</h3>
+            <h3 class="section-title text-center my-5" style="color:#fafafa"><?php echo $arret; ?>T1 - République</h3>
           </div>
           <!-- Passage 1 -->
           <div class="example col-lg-8 col-md-10 ml-auto mr-auto" style="border-radius: .625rem; box-shadow: 0 0.46875rem 2.1875rem rgba(90,97,105,.1), 0 0.9375rem 1.40625rem rgba(90,97,105,.1), 0 0.25rem 0.53125rem rgba(90,97,105,.12), 0 0.125rem 0.1875rem rgba(90,97,105,.1); border: 1px solid;margin-bottom:22px;">
@@ -32,10 +32,10 @@
               <div class="col-md-12">
                 <div class="row">
                   <div class="col col-lg-2">
-                    <img src="images/transports/divia/L3.png" style="margin-top:11px;">
+                    <img src="images/transports/divia/T1.png" style="margin-top:11px;border-radius:.280rem;">
                   </div>
                   <div class="col-6">
-                    <h5 style="color:#fafafa;margin-top:11px;">> Epirey Cap Nord<h4>
+                    <h5 style="color:#fafafa;margin-top:11px;">> DIJON Gare<h4>
                   </div>
                   <div class="col">
                     <h5 style="color:#fafafa;margin-top:11px;text-align:right;font-weight:bold;">18:04<h4>
@@ -51,10 +51,10 @@
               <div class="col-md-12">
                 <div class="row">
                   <div class="col col-lg-2">
-                    <img src="images/transports/divia/L3.png" style="margin-top:11px;">
+                    <img src="images/transports/divia/T1.png" style="margin-top:11px;border-radius:.280rem;">
                   </div>
                   <div class="col-6">
-                    <h5 style="color:#fafafa;margin-top:11px;">> Epirey Cap Nord<h4>
+                    <h5 style="color:#fafafa;margin-top:11px;">> DIJON Gare<h4>
                   </div>
                   <div class="col">
                     <h5 style="color:#fafafa;margin-top:11px;text-align:right;font-weight:bold;">18:17<h4>
@@ -70,10 +70,10 @@
               <div class="col-md-12">
                 <div class="row">
                   <div class="col col-lg-2">
-                    <img src="images/transports/divia/L3.png" style="margin-top:11px;">
+                    <img src="images/transports/divia/T1.png" style="margin-top:11px;border-radius:.280rem;">
                   </div>
                   <div class="col-6">
-                    <h5 style="color:#fafafa;margin-top:11px;">> Epirey Cap Nord<h4>
+                    <h5 style="color:#fafafa;margin-top:11px;">> DIJON Gare<h4>
                   </div>
                   <div class="col">
                     <h5 style="color:#fafafa;margin-top:11px;text-align:right;font-weight:bold;">18:31<h4>
