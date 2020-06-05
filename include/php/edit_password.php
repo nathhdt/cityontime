cd143d694b341359036bce9d4d7565a57fe14e26
qowax <nathan.haudot@outlook.fr>
2020-06-05 12:05:03 +0200

    Corrections bug edit pwd

--- include/php/edit_password.php
+++ include/php/edit_password.php
@@ -5,11 +5,18 @@ if (isset($_POST['type-submit'])) {
     require 'dbh.php';
 
     //Vérification ancien mot de passe
-    if (empty($_POST["oldpw"]) || empty($_POST["newpw"]) || empty($_POST["confnewpw"])) {
-            header('Location: ../../profil.php');
-            exit();
+    $oldPass = $_POST["oldpw"];
+    $newPass = $_POST["newpw"];
+    $confNewPass = $_POST["confnewpw"];
+
+    echo $user . ', ' . $newPass . ', ' . $confNewPass;
+
+
+    if (empty($oldPass) || empty($newPass) || empty($confNewPass)) {
+        header('Location: ../../profil.php');
+        exit();
     } else {
-        $request = "SELECT count(*) FROM cot_users where email = '" . $user . "' and password = '" . $_POST['oldpw'] . "' ";
+        $request = "SELECT count(*) FROM cot_users WHERE email = '" . $user . "' AND password = '" . $oldPass . "' ";
         $exec_request = mysqli_query($connection, $request);
         $reponse      = mysqli_fetch_array($exec_request);
         $count = $reponse['count(*)'];
@@ -17,28 +24,29 @@ if (isset($_POST['type-submit'])) {
         if ($count != 0) // Nom d'utilisateur et mot de passe corrects
             {
                 //Ancien mot de passe ok
+
+                //Vérification que les 2 mdp entrés sont les mêmes
+                if ($newPass == $confNewPass) {
+                    //Les mots de passe correspondent
+
+                    //Si on passe les tests et que tout est ok, exécution modification mdp
+                    //Exécution de la requête
+                    $stmt = $connection->prepare('UPDATE `cot_users` SET `password`= ? WHERE `email`= ?');
+                    $stmt->bind_param('ss', $newPass, $user);
+                    $stmt->execute();
+
+                    //Redirection avec popup
+                    header('Location: ../../profil.php?success=true');
+                } else {
+                    header('Location: ../../profil.php?diff=true');
+                    exit();
+                    //Les mots de passe sont différents !
+                }
             } else {
-                header('Location: ../../profil.php?oldpw=false');
-                //Ancien mot de passe différent !
-            }
+            //Ancien mot de passe différent !
+            header('Location: ../../profil.php?oldpw=false');
+            exit();
+        }
     }
-
-    //Vérification que les 2 mdp entrés sont les mêmes
-    if ($_POST["newpw"] == $_POST["confnewpw"]) {
-        //Les mots de passe correspondent
-    } else {
-        header('Location: ../../profil.php?diff=true');
-        //Les mots de passe sont différents !
-    }
-
-    //Si on passe les tests et que tout est ok, exécution modification mdp
-    //Exécution de la requête
-    $stmt = $connection->prepare('UPDATE `cot_users` SET `password`= ? WHERE `email`= ?');
-    $stmt->bind_param('ss', $_POST["newpw"], $user);
-    $stmt->execute();
-
-    //Redirection avec popup
-    header('Location: ../../profil.php?success=true');
-
 }
 mysqli_close($connection);
