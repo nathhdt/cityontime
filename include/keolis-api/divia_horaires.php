d014788027c461201bef1dcadba82b8323f69da1
qowax <nathan.haudot@outlook.fr>
2020-06-06 12:01:36 +0200

    Horaires en minutes

--- include/keolis-api/divia_horaires.php
+++ include/keolis-api/divia_horaires.php
@@ -4,7 +4,7 @@ $codeville = '217';
 $ref_id = $_GET['ref'];
 
 //Récupération de l'XML par l'API Keolis
-$horaires_data = simplexml_load_file('http://timeo3.keolis.com/relais/217.php?xml=3&refs='.$ref_id.'&ran=1');
+$horaires_data = simplexml_load_file('http://timeo3.keolis.com/relais/' . $codeville . '.php?xml=3&refs=' . $ref_id . '&ran=1');
 
 //Setup variables
 $ligne = '';
@@ -12,15 +12,14 @@ $arret = '';
 $direction = '';
 
 //Liste des lignes & arrpets
-foreach($horaires_data->horaires->horaire->description as $val)
-{
-    $ligne = $horaires_data->horaires->horaire->description->ligne;
-    $arret = $horaires_data->horaires->horaire->description->arret;
-    $direction = $horaires_data->horaires->horaire->description->vers;
-}
+foreach ($horaires_data->horaires as $val) {
+        $ligne = $horaires_data->horaires->horaire->description->ligne;
+        $arret = $horaires_data->horaires->horaire->description->arret;
+        $direction = $horaires_data->horaires->horaire->description->vers;
+    }
 
 //Nombre d'horaires disponibles à afficher
-foreach($horaires_data->horaires->horaire->passages->attributes() as $a => $value) {
+foreach ($horaires_data->horaires->horaire->passages->attributes() as $a => $value) {
     $nombrePassages = $value;
 }
 
@@ -30,6 +29,11 @@ $horaires_xml = array();
 //On récupère les horaires
 $j = 0;
 foreach ($horaires_data->horaires->horaire->passages->passage as $character) {
-    array_push($horaires_xml, $horaires_data->horaires->horaire->passages->passage[$j]->duree);
+    //Conversion de l'heure en minutes avant le temps récup. en XML
+    $now = new DateTime();
+    $heureXML = $horaires_data->horaires->horaire->passages->passage[$j]->duree;
+    $heure = DateTime::createFromFormat('H:i', $heureXML);
+    $minutes = $heure->format('i') - $now->format('i');
+    array_push($horaires_xml, $minutes);
     $j++;
- }
+}
