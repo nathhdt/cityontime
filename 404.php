7c8397ac9150b50b555528a1a217259ffb70e66a
qowax <nathan.haudot@outlook.fr>
2020-06-06 13:00:32 +0200

    Ajout page 404

---
+++ 404.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+if (isset($_SESSION['email'])) {
+  header('Location: index.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <title>CityOnTime - 404 !</title>
+
+  <!-- Header -->
+  <?php include("header.inc"); ?>
+
+</head>
+
+<body class="shards-app-promo-page--1" style="background-color:#212529!important;-webkit-user-select: none;">
+  <div class="welcome d-flex justify-content-center flex-column">
+
+    <!-- Navigation -->
+    <?php include("navbar.inc"); ?>
+
+    <!-- Bloc inscrit -->
+    <div class="inner-wrapper mt-auto mb-auto container">
+      <div class="row">
+        <div class="container mb-5">
+          <div class="section-title col-lg-8 col-md-10 ml-auto mr-auto">
+            <h3 class="section-title text-center my-5" style="color:#fafafa">Oups ! La page que vous cherchez n'existe pas (plus) ! Retournez à l'accueil <a href="index" style="color:#fafafa;text-decoration:underline;">ici</a> !</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Footer -->
+  <?php include("footer.inc"); ?>
+
+</body>
+
+</html>
